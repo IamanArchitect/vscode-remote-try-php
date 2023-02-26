bd64f6b344da31f487483d71daea6977cc0df806
IamanArchitect <116306599+IamanArchitect@users.noreply.github.com>
2023-02-26 11:56:33 +0000

    1. Bearbeitung index.php inkl. include parameter.php

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <?php
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+include "parameters.php";
 
-function sayHello($name) {
-	echo "Hello $name!";
+$persons_in_store = 0;
+
+// GET /filiale/anzahl
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['PATH_INFO'] === '/filiale/anzahl') {
+    echo json_encode(array('anzahl' => $persons_in_store));
 }
 
-?>
+// POST /filiale/erhoehen
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['PATH_INFO'] === '/filiale/erhoehen') {
+    $persons_in_store++;
+    echo json_encode(array('anzahl' => $persons_in_store));
+}
 
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
+// POST /filiale/verringern
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['PATH_INFO'] === '/filiale/verringern') {
+    $persons_in_store--;
+    echo json_encode(array('anzahl' => $persons_in_store));
+}
+?>
