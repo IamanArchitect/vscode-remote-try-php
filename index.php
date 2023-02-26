383b1022cee7fcc18c6bac86ff3db866f4186c4c
IamanArchitect <116306599+IamanArchitect@users.noreply.github.com>
2023-02-26 15:17:53 +0000

    3. Aendern des Serverpfads und Anpassungen des Codes

--- index.php
+++ index.php
@@ -5,18 +5,18 @@ include "parameters.php";
 $persons_in_store = 0;
 
 // GET /counter/counterId
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['PATH_INFO'] === '/counter/counterId') {
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_METHOD'] === '/counter/counterId') {
     echo json_encode(array('counterId' => $persons_in_store));
 }
 
 // POST /counter/increment
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['PATH_INFO'] === '/counter/increment') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_METHOD'] === '/counter/increment') {
     $persons_in_store++;
     echo json_encode(array('counterId' => $persons_in_store));
 }
 
 // POST /counter/decrement
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['PATH_INFO'] === '/counter/decrement') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_METHOD'] === '/counter/decrement') {
     $persons_in_store--;
     echo json_encode(array('counterId' => $persons_in_store));
 }
--- parameters.php
+++ parameters.php
@@ -1,94 +1,146 @@
-swagger: "2.0"
-info:
-  description: ""
-  version: "0.0.1"
-  title: "Corona Counter"
-basePath: "/api/v1"
-tags:
-- name: "counter"
-  description: "Count people entering and leaving a room"
-schemes:
-- "https"
-paths:
-  /counter/{counterId}/value:
-    get:
-      tags:
-      - "counter"
-      summary: "Get counter value"
-      description: "Returns the value for the counter identified by counterId"
-      produces:
-      - "application/json"
-      parameters:
-      - name: "counterId"
-        in: "path"
-        description: "ID of counter"
-        required: true
-        type: "integer"
-        format: "int64"
-      responses:
-        "200":
-          description: "successful operation"
-          schema:
-            $ref: "#/definitions/Value"
-        "400":
-          description: "Invalid ID supplied"
-        "404":
-          description: "Counter not found"
-  /counter/{counterId}/value/increment:
-    post:
-      tags:
-      - "counter"
-      summary: "Increment counter value by 1"
-      description: ""
-      produces:
-      - "application/json"
-      parameters:
-      - name: "counterId"
-        in: "path"
-        description: "ID of counter"
-        required: true
-        type: "integer"
-        format: "int64"
-      responses:
-        "200":
-          description: "successful operation"
-          schema:
-            $ref: "#/definitions/Value"
-        "400":
-          description: "Invalid ID supplied"
-        "404":
-          description: "Counter not found"
-  /counter/{counterId}/value/decrement:
-    post:
-      tags:
-      - "counter"
-      summary: "Decrement counter value by 1"
-      description: ""
-      produces:
-      - "application/json"
-      parameters:
-      - name: "counterId"
-        in: "path"
-        description: "ID of counter"
-        required: true
-        type: "integer"
-        format: "int64"
-      responses:
-        "200":
-          description: "successful operation"
-          schema:
-            $ref: "#/definitions/Value"
-        "400":
-          description: "Invalid ID supplied"
-        "404":
-          description: "Counter not found"
-definitions:
-  Value:
-    type: "object"
-    required:
-    - "value"
-    properties:
-      value:
-        type: "integer"
-        format: "int64"
-        example: 123
+<?php
+
+$json = '{
+    "swagger": "2.0",
+    "info": {
+        "description": "",
+        "version": "0.0.1",
+        "title": "Corona Counter"
+    },
+    "basePath": "/api/v1",
+    "tags": [
+        {
+            "name": "counter",
+            "description": "Count people entering and leaving a room"
+        }
+    ],
+    "schemes": [
+        "https"
+    ],
+    "paths": {
+        "/counter/{counterId}/value": {
+            "get": {
+                "tags": [
+                    "counter"
+                ],
+                "summary": "Get counter value",
+                "description": "Returns the value for the counter identified by counterId",
+                "produces": [
+                    "application/json"
+                ],
+                "parameters": [
+                    {
+                        "name": "counterId",
+                        "in": "path",
+                        "description": "ID of counter",
+                        "required": true,
+                        "type": "integer",
+                        "format": "int64"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "schema": {
+                            "$ref": "#/definitions/Value"
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid ID supplied"
+                    },
+                    "404": {
+                        "description": "Counter not found"
+                    }
+                }
+            },
+            "post": {
+                "tags": [
+                    "counter"
+                ],
+                "summary": "Increment counter value by 1",
+                "description": "",
+                "produces": [
+                    "application/json"
+                ],
+                "parameters": [
+                    {
+                        "name": "counterId",
+                        "in": "path",
+                        "description": "ID of counter",
+                        "required": true,
+                        "type": "integer",
+                        "format": "int64"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "schema": {
+                            "$ref": "#/definitions/Value"
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid ID supplied"
+                    },
+                    "404": {
+                        "description": "Counter not found"
+                    }
+                }
+            }
+        },
+        "/counter/{counterId}/value/decrement": {
+            "post": {
+                "tags": [
+                    "counter"
+                ],
+                "summary": "Decrement counter value by 1",
+                "description": "",
+                "produces": [
+                    "application/json"
+                ],
+                "parameters": [
+                    {
+                        "name": "counterId",
+                        "in": "path",
+                        "description": "ID of counter",
+                        "required": true,
+                        "type": "integer",
+                        "format": "int64"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "schema": {
+                            "$ref": "#/definitions/Value"
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid ID supplied"
+                    },
+                    "404": {
+                        "description": "Counter not found"
+                    }
+                }
+            }
+        }
+    },
+    "definitions": {
+        "Value": {
+            "type": "object",
+            "required": [
+                "value"
+            ],
+            "properties": {
+                "value": {
+                    "type": "integer",
+                    "format": "int64",
+                    "example": 123
+                }
+            }
+        }
+    }
+}';
+
+?>
+
